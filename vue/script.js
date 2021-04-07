0814eb0df268f94886e766218f9314ab38da3896
Matteo <matteomigliori280489@gmail.com>
2021-04-07 08:51:08 +0200

    milestone 1

---
+++ vue/script.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+    
+    el:"#root",
+
+    data:{
+        listMovie:[],
+        srcMovie:'',
+    },
+
+    methods: {
+        search() {
+            const self= this
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=e76246e0c4731c413ba03a3bc5965d13&query=' + this.srcMovie)
+            .then((result) => {
+                
+                this.listMovie = result.data.results;
+                
+            })
+        }
+    }
+    
+        
+
+
+
+
+
+    
+})
