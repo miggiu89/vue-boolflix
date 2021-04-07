87b63daad9e37fe7a34a367f3f337573ed2450c1
Matteo <matteomigliori280489@gmail.com>
2021-04-07 15:50:30 +0200

    push

--- vue/script.js
+++ vue/script.js
@@ -4,20 +4,35 @@ var app = new Vue({
 
     data:{
         listMovie:[],
+        listSeries:[],
         srcMovie:'',
     },
 
     methods: {
         search() {
             const self= this
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=e76246e0c4731c413ba03a3bc5965d13&language=it-IT&include_adult=true&query=' + this.srcMovie)
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=e76246e0c4731c413ba03a3bc5965d13&&include_adult=false&query=' + this.srcMovie)
+            
             .then((result) => {
-                
                 this.listMovie = result.data.results;
-                
+                this.listMovie.forEach(element => {
+                    element.vote_average = Math.floor(parseInt(element.vote_average)/2);
+                });
+            })
+
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=e76246e0c4731c413ba03a3bc5965d13&&include_adult=false&query=' + this.srcMovie)
+            
+            .then((result) => {
+                this.listSeries = result.data.results;
+                this.listSeries.forEach(element => {
+                    element.vote_average = Math.floor(parseInt(element.vote_average)/2);
+                });
             })
         }
-    }
+    },
+
+     
+    
     
         
 
