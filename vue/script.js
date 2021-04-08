fcee441f877dfe47d2ccc2837219c8388ea10d95
Matteo <matteomigliori280489@gmail.com>
2021-04-08 15:53:14 +0200

    milestone 2 e 3 (da fare stelle) e modifica chiamata

--- vue/script.js
+++ vue/script.js
@@ -1,44 +1,51 @@
 var app = new Vue({
-    
-    el:"#root",
-
-    data:{
-        listMovie:[],
-        listSeries:[],
-        srcMovie:'',
+    el:'#root',
+    data: {
+        search:'',
+        list: [],
+        personalApi: 'e76246e0c4731c413ba03a3bc5965d13',
     },
-
     methods: {
-        search() {
-            const self= this
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=e76246e0c4731c413ba03a3bc5965d13&&include_adult=false&query=' + this.srcMovie)
-            
-            .then((result) => {
-                this.listMovie = result.data.results;
-                this.listMovie.forEach(element => {
-                    element.vote_average = Math.floor(parseInt(element.vote_average)/2);
-                });
+        //ricerca film
+        src(){
+            this.list = [];
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params : {
+                    api_key: this.personalApi,
+                    query: this.search,
+                }
+            })
+            .then((res) =>{
+                this.list = this.list.concat(res.data.results);
             })
 
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=e76246e0c4731c413ba03a3bc5965d13&&include_adult=false&query=' + this.srcMovie)
-            
-            .then((result) => {
-                this.listSeries = result.data.results;
-                this.listSeries.forEach(element => {
-                    element.vote_average = Math.floor(parseInt(element.vote_average)/2);
-                });
+            //ricerca serie tv
+            axios.get('https://api.themoviedb.org/3/search/tv',{
+                params : {
+                    api_key: this.personalApi,
+                    query: this.search,
+                }
             })
-        }
-    },
+            .then((res) =>{
+                 this.list = this.list.concat(res.data.results);
+            
+                })
 
-     
-    
-    
-        
+        },
 
+        getFlagSrc: function(flag) {
+           return "https://www.unknown.nu/flags/images/"+ flag + "-100";
+        },
+    }
 
-
-
-
-    
 })
+
+
+
+
+
+
+
+
+
+
